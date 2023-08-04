26eda24ebb497cf990dd35549b727e5e80c8655a
jossanna <johannes.ossanna@gmail.com>
2023-08-04 12:17:13 +0200

    Update Main.py
    Formatting

--- Main.py
+++ Main.py
@@ -8,22 +8,23 @@ st.set_page_config(
 
 st.write("# Welcome to Streamlit! 👋")
 
+
 @st.cache_data
 def load_movies():
     # Load and preprocess your data here
-    movies = pd.read_pickle(
-    'processed_data/movies.pkl.gz')
+    movies = pd.read_pickle('processed_data/movies.pkl.gz')
     # Perform any necessary preprocessing
     return movies
+
 
 movies = load_movies()
 
 
 @st.cache_data
 def load_bo():
-    bo = pd.read_pickle(
-    'processed_data/bo.pkl.gz', parse_dates=['date'])
+    bo = pd.read_pickle('processed_data/bo.pkl.gz', parse_dates=['date'])
     # Perform any necessary preprocessing
     return bo
+
 
 bo = load_bo()

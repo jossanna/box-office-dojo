f268c5d096fa343c42ec1b1a838d8f3a18728c51
jossanna <johannes.ossanna@gmail.com>
2023-08-04 12:14:40 +0200

    Updated imports

--- pages/5_✍️_Crew.py
+++ pages/5_✍️_Crew.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from Start import load_movies, load_bo
+from Main import load_movies, load_bo
 
 st.set_page_config(page_title='Crew', page_icon='🎥',
                    layout="wide", initial_sidebar_state="auto", menu_items=None)
